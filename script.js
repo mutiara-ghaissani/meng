c2505f12f52d6606715e4600b467fe326a5b37d8
mutiara-ghaissani <mutiara.ghaissani@student.alhazen.academy>
2025-05-15 19:18:52 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let angka1 = prompt("masukkan angka pertama:");
+let angka2 = prompt("masukkan angka kedua:");
+
+angka1 = parseFloat(angka1);
+angka2 = parseFloat(angka2);
+
+let hasil1 = angka1 + angka2;
+alert("hasil pertambahan: " + hasil1);
+
+let angka3 = prompt("masukkan angka ketiga:");
+let angka4 = prompt("masukkan angka keempat:");
+
+angka3 = parseFloat(angka3);
+angka4 = parseFloat(angka4);
+
+let hasil2 = angka3 - angka4;
+alert("hasil pengurangan: " + hasil2);
+
+
+let angka5 = prompt("masukkan angka kelima:");
+let angka6 = prompt("masukkan angka keenam :");
+
+angka5 = parseFloat(angka5);
+angka6 = parseFloat(angka6);
+
+let hasil3 = angka5 * angka6;
+alert("hasil perkalian: " + hasil3);
+
+let angka7 = prompt("masukkan angka ketujuh:");
+let angka8 = prompt("masukkan angka kedelapan:");
+
+angka7 = parseFloat(angka7);
+angka8 = parseFloat(angka8);
+
+let hasil4 = angka7 / angka8;
+alert("hasil pembagian: " + hasil4);
+
+
+
+
