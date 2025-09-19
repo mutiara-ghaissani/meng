876bfbefb5187d0efbd77be96763d5b7ccb8bef0
mutiara-ghaissani <mutiara.ghaissani@student.alhazen.academy>
2025-09-19 11:16:57 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,32 @@
-// Function without argument -->
-// Membuat Function
- function greet(){
-    console.log("Helooo, how your life?")
+let btn = document.getElementById("hitungbtn")
+let result = document.getElementById("result")
+
+btn.oneclick = function() {
+    let berat = document.getElementById("Berat Badan").value;
+    let tinggicm = document.getElementById("Tinggi").value;
+
+    berat = parseFloat(berat)
+    tinggicm = parseFloat(tinggicm)
+
+    if (isNaN(berat) || isNaN(tinggicm) || berat <= 0 || tinggicm <= 0) {
+        result.innerHTML = "MASUKKAN NILAI YANG VALID!";
+        return;
+    }
+
+    let tinggiM = tinggicm
+    let bmi = berat / (tinggiM * tinggiM);
+    let kategori = "";
+
+    if (bmi < 18.5) {
+        kategori = "kurus";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        kategori = "normal";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        kategori = "kelebihan berat badan";
+    } else {
+        kategori = "obesitas";
+    }
+
+    result.innerHTML = "BMI kamu: " + bmi.toFixed(2) + "->" + kategori ;
+
 }
-
-// Memanggil Function
-greet()
-
-
-// Function with parameter/argument -->
- function greet(name){
-    alert("Heloo, how your life?" + name + "?")
- }
-
- let yourName = prompt("What's your name :D?")
-
- // Memanggil functiom diikuti parameter
- greet(yourName);
-
- // Function-Return (hny mghslkn nilai, tidak menampilkan apa-apa) -->
- function add(a, b){
-    result = a + b;
-    result_text = a + " + " + b + " = " + result;
-
-    return result_text;
- }
-
- console.log(add(10, 11))
-
- // Function in Variable (kyk return juga tpi ada variable tmbhn jd bisa di output) -->
- function add(a,b){
-    result = a + b;
-    result_text = a + " + " + b + " = " + result;
-
-    return result_text;
- }
-
- addition = add(10, 11);
-
- console.log("The result is" + addition);
