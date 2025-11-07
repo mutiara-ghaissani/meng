2065704a98e4620d814eb1779615a900a86f5db3
mutiara-ghaissani <mutiara.ghaissani@student.alhazen.academy>
2025-11-07 12:21:29 +0700

    Add files via upload

---
+++ assign.py
@@ -0,0 +1,11 @@
+print("STUDENT REGISTRATION ACCOUNT FORM")
+print("---------------------------------")
+
+name = input("Full Name: ")
+email = input("Email: ")
+phone = input("Phone Number: ")
+
+print("Welcome", name, "!")
+print("Congratulation, email", email, "successfully registered in our system ")
+print("Link for login to application sent to number", phone)
+
