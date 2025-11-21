5c74566ba10c172a857ca1adf6971d5b1e20075d
mutiara-ghaissani <mutiara.ghaissani@student.alhazen.academy>
2025-11-21 09:42:11 +0700

    Add files via upload

---
+++ SimpleCalc.py
@@ -0,0 +1,44 @@
+print("Increment Calculator")
+print("--------------------")
+
+a = int(input("Masukkan angka pertama: "))
+b = int(input("Masukkan angka kedua: "))
+result = int(a) + int(b)
+
+print(a, "+", b, "=", result)
+
+print("Subtraction Calculator")
+print("----------------------")
+
+a = int(input("Masukkan angka pertama: "))
+b = int(input("Masukkan angka kedua: "))
+result = int(a) - int(b)
+
+print(a, "-", b, "=", result)
+
+print("Multiplication Calculator")
+print("-------------------------")
+
+a = int(input("Masukkan angka pertama: "))
+b = int(input("Masukkan angka kedua: "))
+result = int(a) * int(b)
+
+print(a, "*", b, "=", result)
+
+print("Division Calculator")
+print("--------------------")
+
+a = int(input("Masukkan angka pertama: "))
+b = int(input("Masukkan angka kedua: "))
+result = int(a) / int(b)
+
+print(a, "/", b, "=", result)
+
+print("Power Calculator")
+print("-----------------")
+
+a = int(input("Masukkan angka pertama: "))
+b = int(input("Masukkan angka kedua: "))
+result = int(a) ** int(b)
+
+print(a, "**", b, "=", result)
