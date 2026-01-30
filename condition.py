4fce3e4579c2b1dce39074e11b261f4760805988
mutiara-ghaissani <mutiara.ghaissani@student.alhazen.academy>
2026-01-30 11:05:55 +0700

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,9 @@
+score = input("Input your score:")
+score = int(score)
+
+if score >= 85:
+    print("Score A")
+elif score >= 70:
+    print("Score B")
+else:
+    print("Score C")
---
+++ loop.py
@@ -0,0 +1,11 @@
+for i in range (1, 6):
+    print(i)
+
+#while
+Amount = input("Input count:")
+Amount = int(Amount)
+
+count = 1
+while count <= Amount:
+    print("Bonjour")
+    count += 1
