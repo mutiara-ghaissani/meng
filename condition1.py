74a0288edc559c012d0e3f9f102b590735b4aad8
mutiara-ghaissani <mutiara.ghaissani@student.alhazen.academy>
2026-02-13 11:03:28 +0700

    Add files via upload

---
+++ condition1.py
@@ -0,0 +1,10 @@
+score = 50
+min_criteria = 75
+
+if score >= min_criteria: 
+    print("Congrats! You pass the exam!")
+elif score < min_criteria:
+    print("Try again! I know you can!")
+else:
+    print("Score not found")
+
---
+++ loop1.py
@@ -0,0 +1,11 @@
+number = 10
+
+while number <= 10:
+    print("I like python")
+    number += 1
+
+numbers = range(10)
+
+# n mrpkn variable baru yg akan menyimpan masing2 item dr iterable object
+for n in numbers:
+    print("I like python", n)
